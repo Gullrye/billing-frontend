90e8b979aa5cf841b5a881321b7ce5029f01af70
Gullrye <wmwspecter@163.com>
2022-11-13 11:21:39 +0800

    feat: 创建 Overlay 组件并添加动画

---
+++ src/shared/Overlay.tsx
@@ -0,0 +1,60 @@
+import { defineComponent, PropType, Transition } from 'vue'
+import { RouterLink } from 'vue-router'
+import { Icon } from './Icon'
+import s from './Overlay.module.less'
+
+export const Overlay = defineComponent({
+  props: {
+    onClose: {
+      type: Function as PropType<() => void>
+    },
+    visible: {
+      type: Boolean,
+      default: false
+    },
+  },
+  setup: (props, context) => {
+    const closeOverlay = () => {
+      props.onClose?.()
+    }
+    const onClickSignIn = () => { }
+    return () => (
+      <div class={s.page}>
+        {props.visible && <div class={s.mask} onClick={closeOverlay}></div>}
+        <Transition name='default-fade'>
+          {
+            props.visible &&
+            <div class={s.overlay}>
+              <section class={s.currentUser} onClick={onClickSignIn}>
+                <h2>未登录用户</h2>
+                <p>点击这里登录</p>
+              </section>
+              <nav>
+                <ul class={s.action_list}>
+                  <li>
+                    <RouterLink to="/statistics" class={s.action}>
+                      <Icon name="charts" class={s.icon} />
+                      <span>统计图表</span>
+                    </RouterLink>
+                  </li>
+                  <li>
+                    <RouterLink to="/export" class={s.action}>
+                      <Icon name="export" class={s.icon} />
+                      <span>导出数据</span>
+                    </RouterLink>
+                  </li>
+                  <li>
+                    <RouterLink to="/notify" class={s.action}>
+                      <Icon name="notify" class={s.icon} />
+                      <span>记账提醒</span>
+                    </RouterLink>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          }
+        </Transition>
+      </div>
+    )
+  }
+})
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,19 +1,33 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { Button } from '../shared/Button'
 import { FloatButton } from '../shared/FloatButton'
 import s from './StartPage.module.less'
+import { NavBar } from '../shared/NavBar'
+import { Overlay } from '../shared/Overlay'
+import { Icon } from '../shared/Icon'
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
+    const overlayVisible = ref(false)
     const onClick = () => {
       console.log('cc')
     }
+    const onClickMenu = () => {
+      overlayVisible.value = true
+    }
     return () => (
       <div>
+        <NavBar>{
+          {
+            default: () => '山竹记账',
+            icon: () => <Icon class={s.navIcon} name='menu' onClick={onClickMenu}></Icon>
+          }
+        }</NavBar>
         <div class={s.button_wrapper}>
           <Button class={s.button} onClick={onClick}>xxx</Button>
         </div>
         <FloatButton iconName='add' />
+        <Overlay onClose={() => overlayVisible.value = false} visible={overlayVisible.value} />
       </div>
     )
   }
