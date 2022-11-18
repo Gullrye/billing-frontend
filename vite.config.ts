7b7063ba8e43e4b846a8d35ed1d9bc4287e9f1c6
Gullrye <wmwspecter@163.com>
2022-11-18 11:53:01 +0800

    feat: 使用vite-plugin-style-import按需引入vant组件

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { svgstorejs } from './src/vite_plugins/svgstore'
+import styleImport, { VantResolve } from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx({ transformOn: true, mergeProps: true }), svgstorejs()]
+  plugins: [
+    vue(),
+    vueJsx({ transformOn: true, mergeProps: true }),
+    svgstorejs(),
+    styleImport({
+      resolves: [VantResolve()],
+    }),
+  ]
 })
