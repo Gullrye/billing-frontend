e43ae8689a1b07da378671e0a532f8eebc3557ed
Gullrye <wmwspecter@163.com>
2022-11-20 16:48:38 +0800

    feat: 数字按键添加删除功能和点击动画

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, ref } from 'vue'
+import { defineComponent, onBeforeUnmount, ref } from 'vue'
 import s from './InputPad.module.less'
 import { Icon } from '../../shared/Icon'
 import { time } from '../../shared/time'
@@ -9,7 +9,21 @@ export const InputPad = defineComponent({
     const now = new Date()
     const currentDate = ref(now)
     const amount = ref('0')
+    const timer = ref<number | undefined>(undefined)
+
+    const currentKey = ref('')
     const appendText = (n: string) => {
+      currentKey.value = n
+      if(n === '清空') {
+        amount.value = '0'
+        return
+      } else if(n === '提交') {
+        return
+      } else if(n === '删除') {
+        amount.value = amount.value.slice(0, -1)
+        !amount.value && (amount.value = '0')
+        return
+      }
       const dotIndex = amount.value.indexOf('.')
       if (amount.value.length >= 13) {
         return
@@ -27,21 +41,7 @@ export const InputPad = defineComponent({
         amount.value += n
       }
     }
-    const buttons = [
-      { text: '1', onClick: () => { appendText('1') } },
-      { text: '2', onClick: () => { appendText('2') } },
-      { text: '3', onClick: () => { appendText('3') } },
-      { text: '4', onClick: () => { appendText('4') } },
-      { text: '5', onClick: () => { appendText('5') } },
-      { text: '6', onClick: () => { appendText('6') } },
-      { text: '7', onClick: () => { appendText('7') } },
-      { text: '8', onClick: () => { appendText('8') } },
-      { text: '9', onClick: () => { appendText('9') } },
-      { text: '.', onClick: () => { appendText('.') } },
-      { text: '0', onClick: () => { appendText('0') } },
-      { text: '清空', onClick: () => { amount.value = '0' } },
-      { text: '提交', onClick: () => { } },
-    ]
+    const buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '0', '清空', '删除', '提交',]
     const pickerVisible = ref(false)
     const showDatePicker = () => {
       pickerVisible.value = true
@@ -53,24 +53,34 @@ export const InputPad = defineComponent({
       currentDate.value = date
       hideDatePicker()
     }
-    return () => <>
-      <div class={s.dateAndAmount}>
-        <span class={s.date} onClick={showDatePicker}>
-          <Icon name="date" class={s.icon} />
-          <span>{time(currentDate.value).format()}</span>
-        </span>
-        <Popup position='bottom' v-model:show={pickerVisible.value}>
-          <DatetimePicker value={currentDate.value} type="date" title="选择年月日" onConfirm={setDate} onCancel={hideDatePicker}
-          min-date={new Date(2022, 0, 1)}
-          />
-        </Popup>
-        <span class={s.amount}>{amount.value}</span>
-      </div>
-      <div class={s.buttons}>
-        {buttons.map(button =>
-          <button onClick={button.onClick}>{button.text}</button>
-        )}
-      </div>
-    </>
+    onBeforeUnmount(() => {
+      timer.value && clearTimeout(timer.value)
+    })
+    return () => {
+      if (currentKey.value) {
+        timer.value = setTimeout(() => {
+          currentKey.value = ''
+        }, 300)
+      }
+      return <>
+        <div class={s.dateAndAmount}>
+          <span class={s.date} onClick={showDatePicker}>
+            <Icon name="date" class={s.icon} />
+            <span>{time(currentDate.value).format()}</span>
+          </span>
+          <Popup position='bottom' v-model:show={pickerVisible.value}>
+            <DatetimePicker value={currentDate.value} type="date" title="选择年月日" onConfirm={setDate} onCancel={hideDatePicker}
+              min-date={new Date(2022, 0, 1)}
+            />
+          </Popup>
+          <span class={s.amount}>{amount.value}</span>
+        </div>
+        <div class={s.buttons}>
+          {buttons.map(button =>
+            <button onClick={()=>appendText(button)} class={currentKey.value === button ? s.active_bg : ''}>{button}</button>
+          )}
+        </div>
+      </>
+    }
   }
 })
